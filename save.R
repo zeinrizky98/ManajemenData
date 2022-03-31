1641f7157f0609260d3d9b82d5631b7b3cdd9746
zeinrizky98 <100661845+zeinrizky98@users.noreply.github.com>
2022-03-31 10:39:19 +0700

    Update save.R

--- save.R
+++ save.R
@@ -1,7 +1,9 @@
 library(RPostgreSQL)
+library(rvest)
+library(dplyr)
 
 query <- '
-CREATE TABLE IF NOT EXISTS BLU (
+CREATE TABLE IF NOT EXISTS LIGA (
   No integer,
   Karakter character(1),
   Angka integer,
@@ -12,11 +14,11 @@ CREATE TABLE IF NOT EXISTS BLU (
 drv <- dbDriver("PostgreSQL")
 
 con <- dbConnect(drv,
-                 dbname = Sys.getenv("ELEPHANT_SQL_DBNAME"), 
-                 host = Sys.getenv("ELEPHANT_SQL_HOST"),
+                 dbname = "ELEPHANT_SQL_DBNAME", 
+                 host = "ELEPHANT_SQL_HOST",
                  port = 5432,
-                 user = Sys.getenv("ELEPHANT_SQL_USER"),
-                 password = Sys.getenv("ELEPHANT_SQL_PASSWORD")
+                 user = "ELEPHANT_SQL_USER",
+                 password = "ELEPHANT_SQL_PASSWORD"
 )
 
 # Membuat Tabel untuk Pertama Kalinya
@@ -25,16 +27,30 @@ data <- dbGetQuery(con, query)
 # Memanggil Tabel, untuk membuat Primary Key nya berurutan.
 
 query2 <- '
-SELECT * FROM "public"."BLU"
+SELECT * FROM "public"."LIGA"
 '
 
 data <- dbGetQuery(con, query2)
 baris <- nrow(data)
 
-data <- data.frame(No = (baris+1):(baris+4),
-                   Karakter = c("A","B","C","D"),
-                   Angka = as.integer(round(rnorm(4,10,10),0)))
+urlprimer <- "https://www.goal.com/id/liga-primer/tabel/2kwbbcootiqqgmrzs6o5inle5"
+primer <- urlprimer %>% read_html() %>% html_table()
 
-dbWriteTable(conn = con, name = "BLU", value = data, append = TRUE, row.names = FALSE, overwrite=FALSE)
+urlserieA <- "https://www.goal.com/id/serie-a/tabel/1r097lpxe0xn03ihb7wi98kao"
+serieA <- urlserieA %>% read_html() %>% html_table()
+
+urllaliga <- "https://www.goal.com/id/laliga/tabel/34pl8szyvrbwcmfkuocjm3r6t"
+laliga <- urllaliga %>% read_html() %>% html_table()
+
+urlbundesliga <- "https://www.goal.com/id/bundesliga/tabel/6by3h89i2eykc341oz7lv1ddd"
+bundesliga <- urlbundesliga %>% read_html() %>% html_table()
+
+data <- data.frame(Minggu = (baris+1),
+                   LigaPrimer = c(primer[[2]][1,2]$Tim[1]),
+                   LigaSerieA = c(serieA[[2]][1,2]$Tim[1]),
+                   LigaLaliga = c(laliga[[2]][1,2]$Tim[1]),
+                   LigaBundesliga = c(bundesliga[[2]][1,2]$Tim[1]))
+
+dbWriteTable(conn = con, name = "LIGA", value = data, append = TRUE, row.names = FALSE, overwrite=FALSE)
 
 on.exit(dbDisconnect(con)) 
