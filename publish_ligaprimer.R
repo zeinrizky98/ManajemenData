b4901dcef5b3a31b7e811649d5f91f19ac168572
zeinrizky98 <100661845+zeinrizky98@users.noreply.github.com>
2022-03-31 12:59:37 +0700

    Update publish_ligaprimer.R

--- publish_ligaprimer.R
+++ publish_ligaprimer.R
@@ -3,41 +3,51 @@ library(rvest)
 library(htmltools)
 library(webshot)
 library(formattable)
+library(RPostgreSQL)
+webshot::install_phantomjs()
 
-# Data
-urlPT <- "https://www.goal.com/id/liga-primer/tabel/2kwbbcootiqqgmrzs6o5inle5"
-data <- urlPT %>% read_html() %>% html_table()
+drv <- dbDriver("PostgreSQL")
 
-# Tabel Klasemen
-export_formattable <- function(f, file, width = "100%", height = NULL, 
-                               background = "white", delay = 0.2)
-{
-  w <- as.htmlwidget(f, width = width, height = height)
-  path <- html_print(w, background = background, viewer = NULL)
-  url <- paste0("file:///", gsub("\\\\", "/", normalizePath(path)))
-  webshot(url,
-          file = file,
-          selector = ".formattable_widget",
-          delay = delay)
-}
+con <- dbConnect(drv,
+                 dbname = "ddykovrz", 
+                 host = "john.db.elephantsql.com",
+                 port = 5432,
+                 user = "ddykovrz",
+                 password = "COG8VZQCXRuhlDEwQEDDmkuQ4iPXAoAF")
 
-## Download the image to a temporary location
-## save to a temp file
-file <- tempfile(fileext = ".png")
+query2 <- '
+SELECT * FROM "public"."primer2" limit 1
+'
 
-p = formattable(data[[2]], list(
-  "Pos" = color_tile("orange", "white"),
-  "+/-" = formatter("span",
-                    style = x ~ style(color = ifelse(x <= 0, "red", "green")))
-))
-export_formattable(
-  p,
-  file,
-  width = "100%",
-  height = "100%",
-  background = "white",
-  delay = 10
-)
+data <- dbGetQuery(con, query2)
+
+# # Tabel Klasemen
+# export_formattable <- function(f, file, width = "100%", height = NULL, 
+#                                background = "white", delay = 0.2)
+# {
+#   w <- as.htmlwidget(f, width = width, height = height)
+#   path <- html_print(w, background = background, viewer = NULL)
+#   url <- paste0("file:///", gsub("\\\\", "/", normalizePath(path)))
+#   webshot(url,
+#           file = file,
+#           selector = ".formattable_widget",
+#           delay = delay)
+# }
+# 
+# ## Download the image to a temporary location
+# ## save to a temp file
+# file <- tempfile(fileext = ".png")
+# 
+# p = formattable(data[[2]])
+# 
+# export_formattable(
+#   p,
+#   file,
+#   width = "100%",
+#   height = "100%",
+#   background = "white",
+#   delay = 10
+# )
 
 ## 1st Hashtag
 hashtag <- c("ManajemenData","ManajemenDataStatistika", "github","rvest","rtweet", "ElephantSQL", "SQL", "bot", "opensource", "ggplot2","PostgreSQL","RPostgreSQL")
@@ -58,15 +68,14 @@ library(rtweet)
 ## Create Twitter token
 bola_token <- rtweet::create_token(
   app = "LigaEropaBOT",
-  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
-  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
-  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  consumer_key =    Sys.getenv("KkOXL26usvkE5Q9XYxZsqISMv"),
+  consumer_secret = Sys.getenv("beaa0xv5rWIuN0Tll6pgase4nl6rCXk9k7XmOZI9eZMVHhLQNT"),
+  access_token =    Sys.getenv("1489083674561683461-4C2bPcSJnArO5W0vFIJsbYWIqaUeAk"),
+  access_secret =   Sys.getenv("LKmpehtvjZ8YwsXam4X2waXW3YB5T56EyGc64SqwcxFz2")
 )
 
 
 ## Post the image to Twitter
 rtweet::post_tweet(
   status = status_details,
-  media = file,
   token = bola_token)
