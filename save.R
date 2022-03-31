63738422ab74bc54d66ef6a5b36fa6f0ea7d7b3f
zeinrizky98 <100661845+zeinrizky98@users.noreply.github.com>
2022-03-31 12:38:33 +0700

    Update save.R

--- save.R
+++ save.R
@@ -14,12 +14,11 @@ CREATE TABLE IF NOT EXISTS LIGA (
 drv <- dbDriver("PostgreSQL")
 
 con <- dbConnect(drv,
-                 dbname = ddykovrz, 
-                 host = john.db.elephantsql.com,
+                 dbname = "ddykovrz", 
+                 host = "john.db.elephantsql.com",
                  port = 5432,
-                 user = ddykovrz,
-                 password = COG8VZQCXRuhlDEwQEDDmkuQ4iPXAoAF
-)
+                 user = "ddykovrz",
+                 password = "COG8VZQCXRuhlDEwQEDDmkuQ4iPXAoAF")
 
 # Membuat Tabel untuk Pertama Kalinya
 data <- dbGetQuery(con, query)
@@ -35,6 +34,7 @@ baris <- nrow(data)
 
 urlprimer <- "https://www.goal.com/id/liga-primer/tabel/2kwbbcootiqqgmrzs6o5inle5"
 primer <- urlprimer %>% read_html() %>% html_table()
+datap <- as.data.frame(primer[[2]])
 
 urlserieA <- "https://www.goal.com/id/serie-a/tabel/1r097lpxe0xn03ihb7wi98kao"
 serieA <- urlserieA %>% read_html() %>% html_table()
@@ -45,12 +45,12 @@ laliga <- urllaliga %>% read_html() %>% html_table()
 urlbundesliga <- "https://www.goal.com/id/bundesliga/tabel/6by3h89i2eykc341oz7lv1ddd"
 bundesliga <- urlbundesliga %>% read_html() %>% html_table()
 
-data <- data.frame(Minggu = (baris+1),
-                   LigaPrimer = c(primer[[2]][1,2]$Tim[1]),
-                   LigaSerieA = c(serieA[[2]][1,2]$Tim[1]),
-                   LigaLaliga = c(laliga[[2]][1,2]$Tim[1]),
-                   LigaBundesliga = c(bundesliga[[2]][1,2]$Tim[1]))
+# data <- data.frame(Minggu = (baris+1),
+#                    LigaPrimer = c(primer[[2]][1,2]$Tim[1]),
+#                    LigaSerieA = c(serieA[[2]][1,2]$Tim[1]),
+#                    LigaLaliga = c(laliga[[2]][1,2]$Tim[1]),
+#                    LigaBundesliga = c(bundesliga[[2]][1,2]$Tim[1]))
 
-dbWriteTable(conn = con, name = "LIGA", value = data, append = TRUE, row.names = FALSE, overwrite=FALSE)
+dbWriteTable(conn = con, name = 'primer2', value = datap, append = TRUE, row.names = FALSE, overwrite=FALSE)
 
 on.exit(dbDisconnect(con)) 
